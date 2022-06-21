72cd3aa05b2592557fbac4f0049321be0df2a3e0
Elias Fekhari <elias.fekhari@edf.fr>
2022-06-21 11:47:45 +0200

    Adding energy services to GreedySupportPoints class, adding example

--- otkerneldesign/GreedySupportPoints.py
+++ otkerneldesign/GreedySupportPoints.py
@@ -7,11 +7,12 @@ Copyright (C) EDF 2022
 """
 import numpy as np
 import openturns as ot
+import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
 
 class GreedySupportPoints:
     """
-    Incrementally select new design points with greedy support points.
+    Incrementally select new design points with greedy support points. Support points use a specific kernel, the energy-distance kernel.
 
     Parameters
     ----------
@@ -44,6 +45,7 @@ class GreedySupportPoints:
         candidate_set=None,
         initial_design=None, 
     ):
+        self._method_label = "support points"
         # Inconsistency
         if candidate_set_size is not None and candidate_set is not None:
             raise ValueError(
@@ -92,6 +94,7 @@ class GreedySupportPoints:
         # Compute distances
         self.distances = self.compute_distance_matrix()
         self._target_potential = self.compute_target_potential()
+        self._target_energy = self.compute_target_energy()
         
     def compute_distance_matrix(self, batch_nb=8):
         """
@@ -160,6 +163,22 @@ class GreedySupportPoints:
         """
         potentials = self.distances.mean(axis=0)
         return potentials
+    
+    def compute_target_energy(self):
+        """
+        Compute the energy of the target probability measure :math:`\\mu`.
+
+        Returns
+        -------
+        potential : float
+                    Energy of the measure :math:`\\mu` defined by
+        
+        .. math::
+            E_{\\mu} := \\int \\int k(\\vect{x}, \\vect{x}') d \\mu(\\vect{x}) d \\mu(\\vect{x}').
+
+        """
+        target_energy = self._target_potential.mean()
+        return target_energy
 
     def compute_current_potential(self, design_indices):
         """
@@ -188,7 +207,30 @@ class GreedySupportPoints:
         current_potential = distances_to_design.mean(axis=1)
         current_potential *= len(design_indices) / (len(design_indices) + 1)
         return current_potential
-    
+
+    def compute_current_energy(self, design_indices):
+        """
+        Compute the energy of the discrete measure defined by the design :math:`\mat{X}_n`.
+        Considering the discrete measure :math:`\\zeta_n = \\frac{1}{n} \\sum_{i=1}^{n} \\delta(\\vect{x}^{(i)})`, its energy is defined as 
+        
+        .. math::
+            E_{\\zeta_n} := \\frac{1}{n^2} \\sum_{i=1}^{n} \\sum_{j=1}^{n} k(\\vect{x}^{(i)}, \\vect{x}^{(j)}).
+
+        Parameters
+        ----------
+        design_indices : list of positive int
+                         List of the indices of the selected points
+                         in the Sample of candidate points
+
+        Returns
+        -------
+        potential : float
+                    Energy of the discrete measure defined by the design
+        """
+        current_potential = self.compute_current_potential(design_indices)
+        current_energy = current_potential.mean()
+        return current_energy
+
     def select_design(self, size):
         """
         Select a design with greedy support points. 
@@ -213,3 +255,34 @@ class GreedySupportPoints:
             self._design_indices = np.append(self._design_indices, next_index)
         sample = self._candidate_set[self._design_indices[self._initial_size:]]
         return sample
+
+    def draw_energy_convergence(self, design_indices):
+        """
+        Draws the convergence of the energy for a set of points selected among the candidate set.
+
+        Parameters
+        ----------
+        design_indices : list of positive int
+                         List of the indices of the selected points
+                         in the Sample of candidate points
+
+        Returns
+        -------
+        fig : matplotlib.Figure
+                    Energy convergence of the design of experiments
+        
+        plot_data : data used to plot the figure
+        """
+        energies = []
+        sizes = range(10, len(design_indices))
+        for i in sizes:
+            energies.append(self.compute_current_energy(design_indices[:i]))
+        fig, ax = plt.subplots(1, figsize=(9, 6))
+        plot_data, = ax.plot(sizes, energies, label=self._method_label)
+        ax.axhline(self._target_energy, color='k', label='target')
+        ax.set_title('Energy convergence')
+        ax.set_xlabel('design size ($n$)')
+        ax.set_ylabel('Energy')
+        ax.legend(loc='best')
+        plt.close()
+        return fig, plot_data
