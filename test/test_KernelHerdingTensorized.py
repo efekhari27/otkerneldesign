a65d1f06cce5a1cc6c01722080f3b46b54fa93b3
josephmure <joseph.mure@edf.fr>
2022-09-07 11:50:15 +0200

    Update unit tests of KernelHerding(Tensorized)

--- test/test_KernelHerdingTensorized.py
+++ test/test_KernelHerdingTensorized.py
@@ -21,10 +21,13 @@ class CheckKernelHerdingKernelHerdingTensorized(unittest.TestCase):
             candidate_set_size=2 ** 12,
             distribution=distribution
         )
-        uniform_design_points, _ = kht.select_design(size)
+        uniform_design_points = kht.select_design(size)
+        smaller_design = kht.select_design(size // 2)
 
         ref = [[0.5, 0.5] ,[0.78125, 0.78125], [0.240967, 0.233643], [0.237061, 0.768799], [0.755859, 0.248047], [0.106201, 0.489502], [0.501465, 0.876465], [0.873535, 0.504395], [0.500488, 0.127441], [0.912109, 0.0917969], [0.639404, 0.647705], [0.0878906, 0.0878906], [0.35376, 0.384521], [0.095459, 0.908447], [0.362061, 0.643799], [0.908203, 0.908203], [0.630859, 0.373047], [0.67749, 0.045166], [0.952881, 0.332275], [0.0705566, 0.667725]]
+
         ott.assert_almost_equal(uniform_design_points, ref, 1e-4, 0.0)
+        ott.assert_almost_equal(smaller_design, ref[:size // 2], 1e-4, 0.0)
 
 
 if __name__ == "__main__":
